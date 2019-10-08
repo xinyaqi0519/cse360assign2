7eb5457a85bb174946c559bc1db05d3f6e3b8f98
Sierra <Sierra@192.168.0.42>
2019-10-08 01:13:19 -0700

    This initial version

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -7,64 +7,47 @@
  * 
  */
 package cse360assign2;
+
 public class AddingMachine {
-	
+
 	private int total;
-	private String history = "0";
-	
 	/**
-	 * @description: This is AddingMachine method which for clarity purpose.
+	 * 
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
 	/**
 	 * 
-	 * @return {int}
-	 * @description: Gets the total.
+	 * @return
 	 */
 	public int getTotal () {
-		return total;
+		return 0;
 	}
-	
 	/**
 	 * 
-	 * @param value {int}
-	 * @description: This method is add the parameter to the total variable.
+	 * @param value
 	 */
 	public void add (int value) {
 		
-		total = total + value;
-		history = history + " + " + value;
-		
 	}
-	
 	/**
 	 * 
-	 * @param value {int}
-	 * @description: This method is subtract the parameter to the total variable.
+	 * @param value
 	 */
 	public void subtract (int value) {
 		
-		total = total - value;
-		history = history + " - " + value;
-		
 	}
-	
 	/**
-	 * 	@description: Return operations in string.
+	 * 	
 	 */
 	public String toString () {
-		return history;
+		return "";
 	}
-	
 	/**
-	 * @description: Clear all operations happened.
+	 * 
 	 */
 	public void clear() {
-		total = 0;
-		history = "0";
-		
+	
 	}
 }
